8e4542e606b279fc0902f863afddffb9065ee967
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-07 20:38:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import App from 'components/App';
+import 'modern-normalize/modern-normalize.css';
+// import './styles.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
+import { theme } from 'constants';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App  />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 );
