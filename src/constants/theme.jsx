8466e681abcc2d297af8d321db7bed8e79a918fe
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-08 22:03:57 +0300

    Create theme.jsx

---
+++ src/constants/theme.jsx
@@ -0,0 +1,59 @@
+export const theme = {
+  colors: {
+    red: '#ff0000',
+    blue: '#0000ff',
+    body: '#e6e6e6',
+    white: '#ffffff',
+    black: '#000000',
+    green: '#00ff00',
+    secondary: '#3f51b5',
+    primary: '#777777',
+    whtBtn: '#e2e2e2',
+    hover: '#303f9f',
+    primaryBtn: '#07c',
+    container: '#bbb5b5',
+  },
+  space: [0, 2, 4, 8, 12, 16, 24, 32, 64, 128],
+
+  fonts: {
+    body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: 'inherit',
+    monospace: 'Menlo, monospace',
+  },
+
+  fontWeights: {
+    body: 400,
+    semiBold: 500,
+    bold: 700,
+  },
+
+  fontSizes: [12, 14, 16, 18, 20, 24, 32, 48, 64, 96],
+
+  lineHeights: {
+    body: 1.5,
+    heading: 1.25,
+  },
+
+  styles: {
+    root: {
+      fontFamily: 'body',
+      fontWeight: 'body',
+      color: 'hover',
+    },
+  },
+
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
+
+  borders: {
+    none: 'none',
+    normal: '1px solid black',
+  },
+
+  shadows: {
+    item: '5px 3px 1px -2px rgba(0, 0, 0, 0.2), 5px 2px 2px 0px rgba(0, 0, 0, 0.4), 5px 1px 5px 0px rgba(0, 0, 0, 0.2)',
+  },
+};
