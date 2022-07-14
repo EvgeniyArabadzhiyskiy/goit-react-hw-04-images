d35375eef341871fc7c94ca6ad03c8179da3ab2e
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-14 21:53:05 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 // import { useReducer } from 'react';
 import { useEffect } from 'react';
 import {  useState } from 'react';
-import { useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import { Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -16,15 +15,15 @@ import Notification from './Notification/Notification';
 
 
 const App = () => {
-  const [page,setPage] = useState(1)
-  const [query, setQuery] = useState('')
-  const [articles, setArticles] = useState([])
-  const [error, setError] = useState(null)
-  const [isloading, setIsloading] = useState(false)
+  const [page,setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [articles, setArticles] = useState([]);
+  const [totalHits, setTotalHits] = useState(false);
+  const [error, setError] = useState(null);
+  const [isloading, setIsloading] = useState(false);
+
+  const CARD_HEIGHT = 260;
   
-
-  const totalHits = useRef(null)
-
   useEffect(() => {
     if (query === '') {
       return
@@ -35,7 +34,7 @@ const App = () => {
         setIsloading(true)
         const imageData = await fetchImages(query, page);
 
-        totalHits.current = imageData.total
+        setTotalHits(imageData.total);
 
         const imagesHits = imageData.hits;
         
@@ -45,20 +44,32 @@ const App = () => {
           return
         }
 
-        setArticles(state => [...state, ...imagesHits ])
+        setArticles(state => [...state, ...imagesHits ]);
 
       } catch (error) {
-        setError(new Error(`Sorry something went wrong. ${error.message}`))
+        setError(new Error(`Sorry something went wrong. ${error.message}`));
 
-        toast.error(`Sorry something went wrong. ${error.message}`,{position: "top-right"})
+        toast.error(`Sorry something went wrong. ${error.message}`,{position: "top-right"});
       } finally {
         setIsloading(false)
+
       }
     }
 
     fetchGalleryImages()
 
   },[query, page])
+
+
+  useEffect(() => {
+    if (page > 1) {
+      window.scrollBy({
+        top: CARD_HEIGHT * 2,
+        behavior: 'smooth'
+      })
+    }
+
+  })
 
   const handleFormSubmit = (query) => {
     setQuery(query)
@@ -71,22 +82,21 @@ const App = () => {
   };
 
   const countImages = articles.length
-  const totalImages = totalHits.current
 
     return (
     <Box display="grid" gridAutoColumns="1fr" gridGap="16px" pb={6} >
 
       <Searchbar  onSearhFormSubmit={handleFormSubmit} page={page} />
-      
+
       {isloading && <Loader />}
 
       {error && <h1 style={{color: 'orangered', textAlign: 'center'}}>{error.message}</h1>}
       
       {countImages > 0 && <ImageGallery articlesHits={articles}   />}
 
-      {(countImages > 0 && countImages < totalImages) && <Button onLoadMore={handleLoadMore} />}
+      {(countImages > 0 && countImages < totalHits) && <Button onLoadMore={handleLoadMore} />}
 
-      {countImages === totalImages && <Notification /> }
+      {countImages === totalHits && countImages > 0 && <Notification /> }
 
       <ToastContainer autoClose={3000}   theme="colored" pauseOnHover  />
     </Box>
@@ -114,36 +124,31 @@ export default App;
 // function galleryReducer(state, {type, payload}) {
 
 //   switch (type) {
-//     case 'loading':
+//     case 'setLoading':
 //       return {
 //         ...state,
 //         isloading: !state.isloading
 //       }
-//     case 'articles':
+//        case 'setTotalHits':
+//       return {
+//         ...state,
+//         totalHits: payload
+//       }
+//     case 'addArticles':
 //       return {
 //         ...state,
 //         articles: [...state.articles, ...payload]
 //       } 
-//      case 'error':
+//      case 'setError':
 //       return {
 //         ...state,
 //         error: new Error(`Sorry something went wrong. ${payload.message}`) 
 //       }  
-//      case 'showModal':
-//       return {
-//         ...state,
-//         showModal: !state.showModal
-//       } 
-//      case 'loadMore':
+//      case 'addNextPage':
 //       return {
 //         ...state,
 //         page: state.page + payload
 //       } 
-//      case 'activIndex':
-//       return {
-//         ...state,
-//         activIndex: state.articles[payload]
-//       }  
 //      case 'serchQuery':
 //       return {
 //         ...state,
@@ -152,12 +157,12 @@ export default App;
 //      case 'resetPage':
 //         return {
 //           ...state,
-//           page: payload
+//           page: 1
 //         } 
 //      case 'resetArticles':
 //         return {
 //           ...state,
-//           articles: payload
+//           articles: []
 //         } 
 //     default:
 //       return state;
@@ -165,22 +170,48 @@ export default App;
 // }
 
 
+// const useGalleryArticles = () => {
 
-// let totalHits = null
-
-// const App = () => {
 //   const [galleryState, dispatch] = useReducer(galleryReducer, {
 //     page: 1,
 //     query: '',
 //     articles: [],
-//     showModal: false,
-//     activIndex: null,
+//     totalHits: null,
 //     isloading: false,
 //     error: null,
 //   })
 
-//   const { page, query, articles, showModal, activIndex, isloading, error } = galleryState
+//   return {
+//     galleryState,
+//     setLoading: () => dispatch({type: "setLoading"}),
+//     setTotalHits: (data) => dispatch({type: 'setTotalHits', payload: data}),
+//     addArticles: (imagesHits) => dispatch({type: 'addArticles', payload: imagesHits}),
+//     setError: (error) => dispatch({type: 'setError', payload: error}),
+//     serchQueryes: (query) => dispatch({type: 'serchQuery', payload: query}),
+//     resetPage: () => dispatch({type: 'resetPage'}),
+//     resetArticles: () => dispatch({type: 'resetArticles'}),
+//     addNextPage: () => dispatch({type: 'addNextPage', payload: 1})
+//   }
 
+
+// }
+
+// const App = () => {
+  
+//   const {
+//     galleryState,
+//     setLoading,
+//     setTotalHits,
+//     addArticles,
+//     setError,
+//     serchQueryes,
+//     resetPage,
+//     resetArticles,
+//     addNextPage} = useGalleryArticles()
+
+//   const { page, query, articles,  isloading, error, totalHits } = galleryState 
+
+  
 //   useEffect(() => {
 //     if (query === '') {
 //       return
@@ -188,11 +219,11 @@ export default App;
 
 //     async function fetchGalleryImages() {
 //       try {
-//         dispatch({type: "loading"})
+//         setLoading()
 
 //         const imageData = await fetchImages(query, page);
 
-//         totalHits = imageData.total
+//         setTotalHits(imageData.total)
 
 //         const imagesHits = imageData.hits;
         
@@ -201,42 +232,32 @@ export default App;
 //            {transition: Zoom, position: "top-center"})
 //           return
 //         }
-
-//         dispatch({type: 'articles', payload: imagesHits})
+//         addArticles(imagesHits)
 
 //       } catch (error) {
-//         dispatch({type: 'error', payload: error})
+//         setError(error)
 
 //         toast.error(`Sorry something went wrong. ${error.message}`,{position: "top-right"})
 //       } finally {
-//         dispatch({type: 'loading'})
+//         setLoading()
 //       }
 //     }
 
 //     fetchGalleryImages()
 
-//   },[query, page])
+//   },[query, page, ])
+//   // [query, page, setLoading, setTotalHits,addArticles, setError]
 
 //   const handleFormSubmit = (query) => {
-//     dispatch({type: 'serchQuery', payload: query})
-//     dispatch({type: 'resetPage', payload: 1})
-//     dispatch({type: 'resetArticles', payload: []})
+//     serchQueryes(query)
+//     resetPage()
+//     resetArticles()
 //   };
 
 //   const handleLoadMore = e => {
-//     dispatch({type: 'loadMore', payload: 1})
+//     addNextPage()
 //   };
 
-//   const toggleModal = () => {
-//     dispatch({type: 'showModal' })
-//   };
-
-//   const handleImageClick = index => {
-//     toggleModal();
-//     return dispatch({type: 'activIndex', payload: index})
-//   };
-
-  
 //   const countImages = articles.length
 
 //     return (
@@ -248,17 +269,11 @@ export default App;
 
 //       {error && <h1 style={{color: 'orangered', textAlign: 'center'}}>{error.message}</h1>}
       
-//       {countImages > 0 && <ImageGallery articlesHits={articles}  onImageClick={handleImageClick} />}
+//       {countImages > 0 && <ImageGallery articlesHits={articles}   />}
 
 //       { (countImages > 0 && countImages < totalHits) && <Button onLoadMore={handleLoadMore} />}
 
-//       {countImages === totalHits && <Notification /> }
-
-//       {showModal && (
-//         <Modal onCloseModal={toggleModal}>
-//           <img src={activIndex.largeImageURL} alt={activIndex.tags} />
-//         </Modal>
-//       )}
+//       {countImages === totalHits && countImages > 0 && <Notification /> }
 
 //       <ToastContainer autoClose={3000}   theme="colored" pauseOnHover  />
 //     </Box>
@@ -271,3 +286,83 @@ export default App;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  =====================================================================================
+// const [galleryState, dispatch] = useReducer(galleryReducer, {
+  //   page: 1,
+  //   query: '',
+  //   articles: [],
+  //   totalHits: null,
+  //   isloading: false,
+  //   error: null,
+  // })
+
+   // dispatch({type: "setLoading"})
+
+   // dispatch({type: 'setTotalHits', payload: imageData.total})
+
+   // dispatch({type: 'addArticles', payload: imagesHits})
+
+   // dispatch({type: 'setError', payload: error})
+
+    // dispatch({type: 'setLoading'})
+
+    // dispatch({type: 'serchQuery', payload: query})
+    // dispatch({type: 'resetPage', payload: 1})
+    // dispatch({type: 'resetArticles', payload: []})
+
+      // dispatch({type: 'addNextPage', payload: 1})
